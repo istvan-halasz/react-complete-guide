7117fd9bca3d53b75c4a1dde94c57897edb16624
Halasz Istvan <ihalasz@pentalog.com>
2021-06-10 19:08:50 +0300

    The Concept of "Composition" ("children props")

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,15 @@
-import './Expenses.css';
-import './ExpenseItem'
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+import './Expenses.css';
 
 const Expenses = (props) => {
     const expenses = props.items.map((item) => (
         <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
     ));
     return (
-        <div className='expenses'>
+        <Card className='expenses'>
             {expenses}
-        </div>
+        </Card>
     );
 }
 
